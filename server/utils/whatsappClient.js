c7a88c675964a2ae438c034e039a0002f4769d8e
unknown <omartv04@gmail.com>
2026-02-10 14:48:02 +0200

    fix: optimize puppeteer args for low-memory cloud environments

--- server/utils/whatsappClient.js
+++ server/utils/whatsappClient.js
@@ -16,7 +16,16 @@ const initializeWhatsApp = () => {
         }),
         puppeteer: {
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', 
+                '--disable-gpu'
+            ]
         }
     });
 
