22eb0be01f08e5ed2bd64cd5fca1507b597c7355
unknown <omartv04@gmail.com>
2026-02-11 02:01:35 +0200

    feat: configure production api url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+// Set base URL for production
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = 'https://fundamental-konstanze-timberzee-0438c2f6.koyeb.app';
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
